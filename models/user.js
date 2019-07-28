3fdaa40e208ba0cd72fee723f2cd98c8de5cfe3f
nogomin <tttime0@gmail.com>
2019-07-28 21:13:15 +0900

    passport 모듈 추가, multer 모듈 추가

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => (
     sequelize.define('user', {
         email: {
             type: DataTypes.STRING(40),
-            allowNull: false,
+            allowNull: true,
             unique: true,
         },
         nick: {
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => (
         },
         password: {
             type: DataTypes.STRING(100),
-            allowNull: false,
+            allowNull: true,
         },
         provider: {
             type: DataTypes.STRING(10),
--- routes/page.js
+++ routes/page.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User } = require('../models');
 
 const router = express.Router();
 
-router.get('/profile', (req, res) => {
-    res.render('profile', { title: '내 정보 - NodeBird', user: null});
+router.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile', { title: '내 정보 - NodeBird', user: req.user});
 });
 
-router.get('/join', (req, res) => {
+router.get('/join', isNotLoggedIn, (req, res) => {
     res.render('join', {
         title: '회원가입 - NodeBird',
-        user: null,
+        user: req.user,
         joinError: req.flash('joinError'),
     });
 });
@@ -18,9 +20,31 @@ router.get('/', (req, res, next) => {
     res.render('main', {
         title: 'NodeBird',
         twits: [],
-        user: null,
+        user: req.user,
         loginError: req.flash('loginError'),
     });
+});
+
+router.get('/', (req, res, next) => {
+    Post.findAll({
+        include: {
+            model: User,
+            attributes: ['id', 'nick'],
+        },
+        order: [['createdAt', 'DESC']],
+    })
+        .then((posts) => {
+            res.render('main', {
+                title: 'NodeBird',
+                twits: posts,
+                user: req.user,
+                loginError: req.flash('loginError'),
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+            next(error);
+        });
 });
 
 module.exports = router;
